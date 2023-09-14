4da486d18bb7f9a4f26b9abccb21a3da2b14cb7f
ansnekit <ansnekit@gmial.com>
2023-09-14 16:23:15 +0300

    actualize db url

--- server/database.ts
+++ server/database.ts
@@ -2,7 +2,7 @@ import { connect } from 'mongoose'
 const password = process.env.DB_PASSWORD
 const dbName = process.env.DB_NAME
 
-const url = `mongodb+srv://admin:${password}@cluster0.n2silvs.mongodb.net/${dbName}?retryWrites=true&w=majority`
+const url = `mongodb+srv://vacancies:${password}@cluster0.lhqo7c7.mongodb.net/${dbName}?retryWrites=true&w=majority`
 export const startConnection = async () => {
     try {
         await connect(url)
